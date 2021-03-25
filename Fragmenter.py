246af952ac9f3bd95423a546808bc97aa8f5064c
Tyler Hatch <tyler1218hatch@gmail.com>
2021-03-24 20:25:00 -0500

    Create Fragmenter.py

---
+++ Fragmenter.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        Fragmenter.py
+# Purpose:     Fragments the Valley Bottom by any inputs
+# Author:      Jordan Gilbert
+#
+# Created:     3/21/21
+#-------------------------------------------------------------------------------
+
+#import modules
+import arcpy
+import sys
+import os
+
+
+def main(valley_bottom, fragmenters, out_folder):
+
+    arcpy.env.overwriteOutput = True
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],)
